ca3109d75cca444dabd005215fd1db32e87f0dc8
lillyphan <lphan24@kentdenver.org>
2023-01-30 09:55:40 -0700

    Test size() and isEmpty()

--- src/testDriver.java
+++ src/testDriver.java
@@ -1,5 +1,7 @@
 public class testDriver {
     public static void main(String[] args) {
-
+        ArrayDictionary<String, Integer> dict = new ArrayDictionary<>();
+        System.out.println("0: " + dict.size());
+        System.out.println("true: " + dict.isEmpty());
     }
 }
